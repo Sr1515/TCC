17bd184ec044a0d8ee4ef9ee7dfd43e32062f9a2
Sr1515 <joao151576@gmail.com>
2025-05-12 13:40:28 -0300

    Adding the navigate for navBar

--- Frontend/src/components/Navbar/index.tsx
+++ Frontend/src/components/Navbar/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Button from "../Button";
 import { Container } from "./style";
 
@@ -13,10 +14,15 @@ const NavBar: React.FC<TitleProps> = ({
     width = "100%",
     height = "6rem"
 }) => {
+    const navigate = useNavigate();
+
+    const handleBack = () => {
+        navigate(-1);
+    };
 
     return (
         <Container color={color} width={width} height={height}>
-            <Button name="Voltar" height="4rem" width="10rem" />
+            <Button name="Voltar" height="4rem" width="10rem" onClick={handleBack} />
         </Container>
     );
 };
